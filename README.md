290a1a131a9c602a1111b118f5974a04657410ca
Lawrence Cilia <cilialawrence@yahoo.co.uk>
2024-07-12 11:05:27 +0100

    add whole dang website wow

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# TechEducatorsWeek2
+# The "As a user..." section:
+## Set of Images that load quickly
+I have three images that load quickly at their highest resolution, 2000x2000. I used Lighthouse to help me optimize them, used ezgif.com to convert jpegs to webps for them.
+
+## Select a thumbnail to display a larger version, with a description
+I do have thumbnails that can be used to display a larger version, however since the example site did not include a description I did not create any sort of thing using javascript, or css.
+
+## Alt text, next & previous buttons via keyboard.
+I do have alternative text, as well as title text for the images. Images can be loaded sequentially (next, prevous) using the keyboard (left and right arrows, "," and "." keys).
+
+# Requirements met
+
+### One page, with a set of thumbnail images, and a larger image
+### Styled appropriately with CSS, and make use of media queries
+### Use client-side JavaScript to display the larger image when a thumbnail is selected
+### Use client-side JavaScript to navigate between images using the keyboard (tab, enter)
+
+# Stretch Goals met
+
+### Handle user interaction with keyboard only 
+Does not use Tab and Enter as they have native functionality in most browsers, so used other keys instead.
+
+### Use your operating system's voiceover tools or a Screen Reader to have the computer announce the alt text of the selected image.
+Hidden <p> tag exists, is updated so narrator will say the name of the image when clicked.
+
+# Difficulties
+
+Getting efficient image loading was very difficult for me. I had to use mutliple different converters online to get my .jepg files to convert nicely into .webp files, as the filetype seems to have a lot of metadata that greatly alters the state of the image.
---
+++ script.js
@@ -0,0 +1,97 @@
+const displayImg = document.getElementById("DisplayImg");
+const birdBtn = document.getElementById("BirdImg");
+const treeBtn = document.getElementById("TreeImg");
+const desertBtn = document.getElementById("DesertImg");
+const announcer = document.getElementById("announcer");
+
+const images ={
+    imageArray: [
+        {src: "./images/birb/2000x2000.webp", alt:"Bird walking on a Beach during the sunset.",
+            srcset:`
+            ./images/birb/2000x2000.webp 2000w,
+            ./images/birb/1000x1000.webp 1000w,
+            ./images/birb/500x500.webp 500w,
+            `
+        },
+        {src: "./images/tree/2000x2000.webp", alt:"Tree.",
+            srcset:`
+            ./images/tree/2000x2000.webp 2000w,
+            ./images/tree/1000x1000.webp 1000w,
+            ./images/tree/500x500.webp 500w,
+            `
+        },
+        {src: "./images/desert/2000x2000.webp", alt:"Desert at night. Coarse and rough.", 
+            srcset:`
+            ./images/desert/2000x2000.webp 2000w,
+            ./images/desert/1000x1000.webp 1000w,
+            ./images/desert/500x500.webp 500w,
+            `
+        },
+    ],
+    imageIndex: 0,
+    updateDisplay: function(index){
+        displayImg.src = this.imageArray[index].src;
+        displayImg.alt = this.imageArray[index].alt;
+        displayImg.srcset = this.imageArray[index].srcset;
+    },
+    setIndex: function(newIndex){
+        if(newIndex == this.imageIndex){
+            return;
+        }
+
+        if(newIndex < 0){
+            newIndex = this.imageArray.length-1;
+        }
+        else if(newIndex > this.imageArray.length-1){
+            newIndex = 0;
+        }
+        this.imageIndex = newIndex;
+        this.updateDisplay(this.imageIndex);
+    },
+    goBack: function(){
+        this.setIndex(this.imageIndex-1);
+    },
+    goForward: function(){
+        this.setIndex(this.imageIndex+1);
+    },
+}
+
+birdBtn.addEventListener("click", function(){
+    images.setIndex(0);
+});
+treeBtn.addEventListener("click",  function(){
+    images.setIndex(1);
+});
+desertBtn.addEventListener("click",  function(){
+    images.setIndex(2);
+});
+
+birdBtn.addEventListener("focus", function(){
+    images.setIndex(0);
+});
+treeBtn.addEventListener("focus",  function(){
+    images.setIndex(1);
+});
+desertBtn.addEventListener("focus",  function(){
+    images.setIndex(2);
+});
+
+birdBtn.addEventListener("mouseover", function(){
+    announcer.textContent = birdBtn.alt;
+    console.log("hover over birdbtn");
+});
+treeBtn.addEventListener("mouseover",  function(){
+    announcer.textContent = treeBtn.alt;
+});
+desertBtn.addEventListener("mouseover",  function(){
+    announcer.textContent = desertBtn.alt;
+});
+
+document.addEventListener("keydown", function(event){
+    if(event.key == "ArrowRight" || event.key == "."){
+        images.goForward();
+    }
+    if(event.key == "ArrowLeft"|| event.key == ","){
+        images.goBack();
+    }
+});
